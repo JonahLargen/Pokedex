83df7c8ae871f2ae7783942218a046c33d96f9fd
Jonah Largen <jhlargen2@gmail.com>
2025-06-16 20:35:07 -0400

    REPL Loop

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex> ")
+		if scanner.Scan() {
+			input := scanner.Text()
+			cleanedInput := cleanInput(input)
+			if len(cleanedInput) == 0 {
+				fmt.Println("No input provided. Please enter a command.")
+				continue
+			}
+			firstWord := cleanedInput[0]
+			fmt.Printf("Your command was: %v\n", firstWord)
+		} else {
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Error reading input: %v\n", err)
+			}
+			break
+		}
+	}
 }
